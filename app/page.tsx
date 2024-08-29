16419a1e26a73657733f3ea71c07d3d2eebe2826
Padmajothi <pathimoolam@prometheussoftware.ca>
2024-08-29 15:09:15 -0400

    Initial Project setup - tailwind setup and dependencies installed

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-white marker:text-3xl underline">Health Connect</h1>
+    <Button>Click</Button>
+   </div>
+  );
+}
