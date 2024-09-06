960c88f2c660d4acfa845c0aef725a0599b08476
Padmajothi <pathimoolam@prometheussoftware.ca>
2024-09-06 10:02:13 -0400

    Rearranged the imports order

--- components/ui/input-otp.tsx
+++ components/ui/input-otp.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { OTPInput, OTPInputContext } from "input-otp"
-import * as React from "react"
 import { Dot } from "lucide-react"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
