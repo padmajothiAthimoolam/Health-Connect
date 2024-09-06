a31fe0bad0e51aa099b1cf20b33d5dc85b18b49e
Padmajothi <pathimoolam@prometheussoftware.ca>
2024-09-06 09:35:48 -0400

    Rearranged the imports order

--- components/ui/input-otp.tsx
+++ components/ui/input-otp.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as React from "react"
 import { OTPInput, OTPInputContext } from "input-otp"
+import * as React from "react"
 import { Dot } from "lucide-react"
 
 import { cn } from "@/lib/utils"
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // typescript: {
-    //     ignoreBuildErrors: true,
-    // },
-    // eslint: {
-    //     ignoreBuildErrors: true,
-    // }
 };
 
 export default withSentryConfig(withSentryConfig(
